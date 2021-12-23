0dd09b4cd69dcf40af0a6e16c2bb9adc715d79f3
sushmita12345 <38626023+sushmita12345@users.noreply.github.com>
2021-12-23 23:11:43 +0530

    Airbnb project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import userData from "./data"
 import './App.css';
 
 function App() {
+  const card = userData.map(item => {
+    return (
+      <Card
+        key={item.id}
+        item={item}
+      />
+    )
+
+
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {card}
+      </section>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react" 
+import airbnb from "../images/airbnb.png"
+
+ 
+ export default function Navbar() {
+     return (
+         <nav>
+            <img src={airbnb} alt="logo" className="nav-logo"/>
+        </nav>
+     )
+ }
